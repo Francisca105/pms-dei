0d08176ca148de2090ddf28a3c4b4eca52f95491
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-03-14 12:28:38 +0000

    fix: update button label in DeletePersonDialog to clarify action

--- src/frontend/src/views/people/DeletePersonDialog.vue
+++ src/frontend/src/views/people/DeletePersonDialog.vue
@@ -17,9 +17,9 @@
       <v-divider></v-divider>
 
       <v-card-actions>
-        <v-spacer></v-spacer>
         <v-btn text @click="closeDialog">Cancel</v-btn>
-        <v-btn color="primary" text @click="deletePerson">Save</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="deletePerson">Delete</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
