4613f7215566a337351c7d735a41780846c6442d
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-03-15 16:40:04 +0000

    feat: create PeopleTable component and update PeopleView to utilize it

---
+++ src/frontend/src/views/people/PeopleTable.vue
@@ -0,0 +1,166 @@
+<template>
+  <v-row align="center">
+    <v-col>
+      <h2 class="text-left ml-1">Listagem de Pessoas</h2>
+    </v-col>
+    <v-col cols="auto">
+      <CreatePersonDialog @person-created="getPeople" />
+    </v-col>
+  </v-row>
+
+  <v-text-field
+    v-model="search"
+    label="Search"
+    prepend-inner-icon="mdi-magnify"
+    variant="outlined"
+    hide-details
+    single-line
+  ></v-text-field>
+
+  <v-data-table
+    :headers="headers"
+    :items="people"
+    :search="search"
+    :loading="loading"
+    :custom-filter="fuzzySearch"
+    item-key="id"
+    class="text-left"
+    no-data-text="Sem pessoas a apresentar."
+  >
+    <template v-slot:[`item.type`]="{ item }">
+      <v-chip v-if="item.type === 'COORDINATOR'" color="purple" text-color="white">
+        Coordenador
+      </v-chip>
+      <v-chip v-else-if="item.type === 'STAFF'" color="red" text-color="white"> Staff </v-chip>
+      <v-chip v-else-if="item.type === 'TEACHER'" color="blue" text-color="white">
+        Professor
+      </v-chip>
+      <v-chip v-else-if="item.type === 'SC'" color="orange" text-color="white"> SC </v-chip>
+      <v-chip v-else color="green" text-color="white"> Aluno </v-chip>
+    </template>
+    <template v-slot:[`item.actions`]="{ item }">
+      <v-icon @click="editPerson(item)" class="mr-2">mdi-pencil</v-icon>
+      <v-icon @click="deletePerson(item)">mdi-delete</v-icon>
+    </template>
+  </v-data-table>
+
+  <EditPersonDialog
+    v-if="editDialog"
+    :person="toEditPerson"
+    @person-edited="updatePersonInList"
+    @close-edit-dialog="closeEditDialog"
+  />
+
+  <DeletePersonDialog
+    v-if="deleteDialog"
+    :person="toDeletePerson"
+    @person-deleted="deletePersonInList"
+    @close-delete-dialog="closeDeleteDialog"
+  />
+</template>
+
+<script setup lang="ts">
+import type PeopleDto from '@/models/PeopleDto'
+import RemoteService from '@/services/RemoteService'
+import CreatePersonDialog from './CreatePersonDialog.vue'
+import { reactive, ref } from 'vue'
+import EditPersonDialog from './EditPersonDialog.vue'
+import DeletePersonDialog from './DeletePersonDialog.vue'
+
+let search = ref('')
+let loading = ref(true)
+let editDialog = ref(false)
+let deleteDialog = ref(false)
+
+const headers = [
+  { title: 'ID', key: 'id', value: 'id', sortable: true, filterable: false },
+  {
+    title: 'Nome',
+    key: 'name',
+    value: 'name',
+    sortable: true,
+    filterable: true
+  },
+  {
+    title: 'IST ID',
+    key: 'istId',
+    value: 'istId',
+    sortable: true,
+    filterable: true
+  },
+  {
+    title: 'Email',
+    key: 'email',
+    value: 'email',
+    sortable: true,
+    filterable: true
+  },
+  {
+    title: 'Tipo',
+    key: 'type',
+    value: 'type',
+    sortable: true,
+    filterable: true
+  },
+  {
+    title: 'Ações',
+    key: 'actions',
+    value: 'actions',
+    sortable: false,
+    filterable: false
+  }
+  // TODO: maybe add another column with possible actions? (edit / delete)
+]
+
+const toEditPerson = ref<PeopleDto | null>(null)
+const toDeletePerson = ref<PeopleDto | null>(null)
+let people: PeopleDto[] = reactive([])
+
+getPeople()
+async function getPeople() {
+  people.splice(0, people.length)
+  people.push(...(await RemoteService.getPeople()))
+  loading.value = false
+}
+
+const editPerson = (person: PeopleDto) => {
+  toEditPerson.value = { ...person }
+  editDialog.value = true
+}
+
+const closeEditDialog = () => {
+  editDialog.value = false
+}
+
+const updatePersonInList = (updatedPerson: PeopleDto) => {
+  const index = people.findIndex((person) => person.id === updatedPerson.id)
+  if (index !== -1) {
+    people[index] = { ...updatedPerson }
+  }
+  editDialog.value = false
+}
+
+const deletePerson = (person: PeopleDto) => {
+  toDeletePerson.value = { ...person }
+  deleteDialog.value = true
+}
+
+const closeDeleteDialog = () => {
+  deleteDialog.value = false
+}
+
+const deletePersonInList = (deletedPerson: PeopleDto) => {
+  const index = people.findIndex((person) => person.id === deletedPerson.id)
+  if (index !== -1) {
+    people.splice(index, 1)
+  }
+  deleteDialog.value = false
+}
+
+const fuzzySearch = (value: string, search: string) => {
+  // Regex to match any character in between the search characters
+  if (!value) return false
+  let searchRegex = new RegExp(search.split('').join('.*'), 'i')
+  return searchRegex.test(value)
+}
+</script>
