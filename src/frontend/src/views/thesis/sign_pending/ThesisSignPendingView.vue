edcda3944b3aa253eb217ba573d43f0ddf9a212c
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-03-16 03:00:05 +0000

    feat: add Thesis Sign Pending view with role-based access control

---
+++ src/frontend/src/views/thesis/sign_pending/ThesisSignPendingView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="roleStore.currentRole === 'coordinator'">
+    <SignTable />
+  </div>
+  <div v-else>
+    <Error403 />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRoleStore } from '@/stores/role'
+import Error403 from '@/components/errors/Error403.vue'
+import SignTable from './ThesisSignPending.vue'
+
+const roleStore = useRoleStore()
+</script>
