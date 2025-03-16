47f2947f3092439c8d4c12ce74b9c0d03390103a
Francisca Almeida <francisca.vicente.de.almeida@tecnico.ulisboa.pt>
2025-03-16 09:27:48 +0000

    refactor: update DefenseView to streamline role-based content and improve clarity

--- src/frontend/src/views/defense/DefenseView.vue
+++ src/frontend/src/views/defense/DefenseView.vue
@@ -1,37 +1,12 @@
 <template>
   <div class="text-center">
-    <h1>Teses</h1>
+    <h1>Defesa</h1>
 
     <br />
 
-    <div v-if="roleStore.currentRole === 'student'">
-      <h2>Área do Estudante</h2>
-      <p>Aqui pode gerir a sua tese e defesa.</p>
-      <router-link to="/students">Ver o Meu Progresso</router-link>
-    </div>
-
-    <div v-else-if="roleStore.currentRole === 'coordinator'">
+    <div v-if="roleStore.currentRole === 'coordinator'">
       <h2>Área do Coordenador</h2>
-      <p>Aqui pode aprovar propostas, agendar defesas e gerir workflows.</p>
-      <router-link to="/students">Gerir Estudantes</router-link>
-    </div>
-
-    <div v-else-if="roleStore.currentRole === 'staff'">
-      <h2>Área do Staff</h2>
-      <p>Aqui pode submeter documentos ao Fenix.</p>
-      <router-link to="/students">Ver Estudantes</router-link>
-    </div>
-
-    <div v-else-if="roleStore.currentRole === 'admin'">
-      <h2>Área do SC (Scientific Committee)</h2>
-      <p>Aqui pode aprovar propostas de júri.</p>
-      <router-link to="/students">Ver Propostas</router-link>
-    </div>
-
-    <div v-else-if="roleStore.currentRole === 'teacher'">
-      <h2>Área do Professor</h2>
-      <p>Aqui pode participar em júris e revisões.</p>
-      <router-link to="/students">Ver Júris</router-link>
+      <router-link to="/students">A gestão de uma defesa é feita de forma singular, entra numa defesa de um estudante e pode editar a partir daí.</router-link>
     </div>
 
     <div v-else>
@@ -44,18 +19,6 @@
 
 <script setup lang="ts">
 import { useRoleStore } from '@/stores/role'
-import RemoteService from '@/services/RemoteService'
-import ThesisWorkflowDto from '@/models/ThesisWorkflowDto'
-import { reactive, ref } from 'vue'
-
-let thesis: ThesisWorkflowDto[] = reactive([])
-
-getThesis()
-async function getThesis() {
-  thesis.splice(0, thesis.length)
-  thesis.push(...(await RemoteService.getThesis()))
-  console.log(thesis.values)
-}
 
 const roleStore = useRoleStore()
 </script>
